85904c79c7fc2cded7eaccf3b8bc316028928644
Phạm Trương Minh Đức <phamtruongminhduc@gmail.com>
2021-10-11 18:29:57 +0700

    remove unnecess file

--- src/assignment.php
+++ src/assignment.php
@@ -1,9 +1,66 @@
 <?php
     echo file_get_contents('header.html');
     if ($SESSION['role'] == true) { /* FIXME: check role */
-        echo file_get_contents ("tAssignment.php");
+        echo <<<CODE
+        <div class="row">
+            <div class="col-md">
+                <div class="upload-form">
+                    <form action="upload.php" method="POST" enctype= "multipart/form-data">
+                        <div class="form-group">
+                            <h2 class="text-center">Upload Assignment</h2>  
+                            <div class="form-group">
+                                <input type="file" class="form-control-file" name="fileUpload" require="required">
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block">Upload</button> 
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="upload-form">
+                    <div class="form-group"><br>
+                        <h2 class="text-center">Assignment Management</h2>  <br>
+                        <a href="view_assignment.php">
+                            <button type="submit" class="btn btn-primary btn-block">Student's Assignment</button> 
+                        </a>
+                        <br>
+                        <a href="my_assignment.php">
+                            <button type="submit" class="btn btn-primary btn-block">My Assignment</button> 
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        CODE;
     } else {
-        echo file_get_contents ("sAssignment.php");
+        echo <<<CODE
+        <div class="row">
+            <div class="col-md">
+                <div class="upload-form">
+                    <div class="form-group">
+                        <form action="view_assignment.php" method="POST" enctype= "multipart/form-data">
+                            <h2 class="text-center">Assignment Management</h2>  
+                            <br><br>
+                            <button type="submit" class="btn btn-primary btn-block">View Assignment</button> 
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="upload-form">
+                    <form action="upload.php" method="POST" enctype= "multipart/form-data">
+                        <div class="form-group">
+                            <h2 class="text-center">Answer</h2>  
+                            <div class="form-group">
+                                <input type="file" class="form-control-file" name="fileUpload" require="required">
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block">Upload</button> 
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        CODE;
     }
 ?>  
 
