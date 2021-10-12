f063b2cd006b9032cf5ba1965c42fc0e3f6162fa
Phạm Trương Minh Đức <phamtruongminhduc@gmail.com>
2021-10-12 18:22:30 +0700

    foo

--- src/profile.php
+++ src/profile.php
@@ -8,7 +8,7 @@ echo file_get_contents ('header.html');
 echo "<header><title>Profile</title></header>";
 ?>
 <div class="login-form">
-    <form action="update.php" method="POST">
+    <form action="profile.php" method="POST">
         <h2 class="text-center">Update Profile</h2>
         <div class="form-group">
             <input type="text" class="form-control" name="username" value="<?=$_SESSION['name']?>" disabled>
@@ -28,9 +28,12 @@ echo "<header><title>Profile</title></header>";
         <button type="submit" class="btn btn-primary btn-block" id="updateButton">Update Information</button>              
     </form>
     <a href="index.php">
-    <button type="submit" class="btn btn-primary btn-block">Back to dashboard</button>
+    <button type="submit" class="btn btn-primary btn-block">Back to Dashboard</button>
     </a>
 </div>
 <?php
+/* include 'connection.php';
+
+echo "<script>alert('Update profile successfully!'); window.location = './register.php';</script>"; */
 echo file_get_contents ('footer.html'); 
 ?>
