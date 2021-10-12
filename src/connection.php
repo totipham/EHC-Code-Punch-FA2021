bd09208eaac3ed37420bc87407ff432f45eedd92
Phạm Trương Minh Đức <phamtruongminhduc@gmail.com>
2021-10-12 17:46:36 +0700

    serverDB -> server

--- src/connection.php
+++ src/connection.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-$serverDB='localhost';
+$server='localhost';
 $usernameDB='root';
 $passwordDB='';
 $nameDB='users';
 
-$con = mysqli_connect($serverDB, $usernameDB, $passwordDB, $nameDB);
+$con = mysqli_connect($server, $usernameDB, $passwordDB, $nameDB);
 if (mysqli_connect_errno ()){
     exit ("Failed to connect to MySQL ".  mysqli_connect_error());
 }
