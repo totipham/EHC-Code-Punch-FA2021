86f8621d690d7dccd3cfaea4ed0c1a4971996018
Phạm Trương Minh Đức <phamtruongminhduc@gmail.com>
2021-10-12 17:23:37 +0700

    fix account table

--- sql/database.sql
+++ sql/database.sql
@@ -1,29 +1,31 @@
-CREATE DATABASE test
+CREATE DATABASE users
 
-CREATE TABLE Account(
-    id int NOT NULL PRIMARY KEY,
-    username varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    fullName varchar(255) NOT NULL,
-    email nvarchar(255) NOT NULL,
-    phone int,   
-);
-CREATE TABLE Message(
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `role` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE message(
 	idMess int NOT NULL PRIMARY KEY,
-	id int NOT NULL references Account(id),
+	id int NOT NULL references account(id),
 	sender varchar(255) NOT NULL,
 	content varchar(255),
 );
-CREATE TABLE Game(
+CREATE TABLE game(
 	challID varchar(255) NOT NULL PRIMARY KEY,
 	challenge varchar(255) NOT NULL,
 	hint varchar(255),
-	id int references Account(id),
+	id int references account(id),
 );
-INSERT INTO Account(id,username,password,fullName,email,phone)
+INSERT INTO account(id,username,password,fullName,email,phone)
 VALUES ('1', 'quynhnn', '1234', 'nguyen nhu quynh', 'quynhnnhe140094@fpt.edu.vn', '01231231234');
 INSERT INTO Message(idMess,id,sender,content)
 VALUES ('1', '1', 'quynhnn', 'helloworld');
-INSERT INTO Game(challID, challenge, hint, id)
+INSERT INTO game(challID, challenge, hint, id)
 VALUES ('1', 'acm', 'tungdlm', '1');
 
