54c59ce8a53b2eedc73f14ec4e3535e8e0aadd32
Phạm Trương Minh Đức <phamtruongminhduc@gmail.com>
2021-10-12 17:33:02 +0700

    change page title

--- src/login.php
+++ src/login.php
@@ -5,6 +5,7 @@ if (isset($_SESSION['loggedin']) == true) {
 	exit;
 }
 echo file_get_contents ('header.html');
+echo "<header><title>Login</title></header>";
 echo <<<CODE
     <div class="login-form">
         <form action="authentication.php" method="post">
