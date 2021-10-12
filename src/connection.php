fba8417995b5ca83da8a555b24f32f162c8ab74d
Phạm Trương Minh Đức <phamtruongminhduc@gmail.com>
2021-10-12 17:29:44 +0700

    remove unnescess char

--- src/connection.php
+++ src/connection.php
@@ -10,6 +10,5 @@ if (mysqli_connect_errno ()){
     exit ("Failed to connect to MySQL ".  mysqli_connect_error());
 }
 if ( !isset($_POST['username'], $_POST['password']) ) {
-	// Could not get the data that should have been sent.
 	exit('Please fill both the username and password fields!');
 }
