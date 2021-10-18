619a818f9394784203cd5366097357aff7f05aa8
Nguyễn Hoàng Minh <minhbk58@gmail.com>
2021-10-18 17:07:51 +0700

    Update delete.php

--- src/delete.php
+++ src/delete.php
@@ -2,13 +2,21 @@
 require 'connection.php';
 $conn = dbConnect::ConnectToDB();
 $stuID = $_GET['ID'];
-
-$sql = "DELETE FROM account WHERE id='$stuID';";
-if(!mysqli_query($conn, $sql)){
-    die('Failed: '.mysqli_error($conn));
+$check = "SELECT * FROM account WHERE id='$stuID';";
+if (!mysqli_query($conn, $check)) {
+    die('Failed: ' . mysqli_error($conn));
+} else {
+    $info = mysqli_fetch_assoc(mysqli_query($conn, $check));
+    if ($info['role'] == 1) {
+        echo "<script>alert('Can not delete a teacher'); window.location = 'studentInfo.php';</script>";
+    } else {
+        $sql = "DELETE FROM account WHERE id='$stuID';";
+        if (!mysqli_query($conn, $sql)) {
+            die('Failed: ' . mysqli_error($conn));
+        }
+        echo "<script>alert('Deleted'); window.location = 'studentInfo.php';</script>";
+    }
 }
-echo "Done";
-header("Refresh:4, location: studentInfo.php")
 ?>
-<br/>
+<br />
 <a href="studentInfo.php">Back</a>
