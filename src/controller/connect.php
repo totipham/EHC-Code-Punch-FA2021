c1d5bf4dd4728d18e826105900a536c3ca35675d
Phạm Trương Minh Đức <phamtruongminhduc@gmail.com>
2021-10-22 21:37:33 +0700

    switch to sqlite

--- src/controller/connect.php
+++ src/controller/connect.php
@@ -2,15 +2,17 @@
 
 class dbConnect {
     public static function ConnectToDB() {
-        $server='localhost';
+        /* $server='localhost';
         $usernameDB='root';
         $passwordDB='';
         $nameDB='users';
-        return mysqli_connect($server, $usernameDB, $passwordDB, $nameDB);
+        return mysqli_connect($server, $usernameDB, $passwordDB, $nameDB); */
+        $cur_dir = getcwd();
+        return new PDO('sqlite:D:\\xampp\\htdocs\\CodePunch\\src\\models\\database.db');
     }
 
     public static function Disconnect($con) {
-        mysqli_close($con);
+        return null;
     }
 }
 
