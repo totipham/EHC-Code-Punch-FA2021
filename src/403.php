2a3f3756b72dc1fdfc600696015f6e5b896dcb60
Phạm Trương Minh Đức <phamtruongminhduc@gmail.com>
2021-10-24 16:09:58 +0700

    modify htaccess

---
+++ src/403.php
@@ -0,0 +1,11 @@
+<?php
+echo file_get_contents ('views/header.php');
+?>
+<header><title>403</title></header>
+<div class="card shadow-lg">
+    <div class="card-body p-5">
+        <h1>403</h1>
+    </div>
+</div>
+
+<?php echo file_get_contents ('views/footer.php'); ?>
