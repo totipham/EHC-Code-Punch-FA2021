a640a783541d3f8ce8a4974abbfed8c682f0519e
Phạm Trương Minh Đức <phamtruongminhduc@gmail.com>
2021-10-24 15:42:29 +0700

    update ui

--- src/assignment.php
+++ src/assignment.php
@@ -26,33 +26,39 @@ if ($checkPermission->isTeacher() == 1): ?>
     <div class="row"> 
         <div class="col-md">
             <div style="margin: 50px auto;">
-                <form class="form" action="controller/upload.php" method="POST" enctype= "multipart/form-data">
-                    <div class="form-group">
-                        <h2 class="text-center">Upload Assignment</h2>  
-                        <input type="text" name="uploadAssignment" value="1" hidden>
-                        <div class="form-group">
-                            <input type="text" class="form-control-file" name="assName" placeholder="Assignment Name" required="required">
-                        </div>
-                        <div class="form-group">
-                            <input type="file" class="form-control-file" name="assUpload" required>
-                        </div>
-                        <button type="submit" class="btn btn-primary btn-block">Upload</button> 
+                <div class="card shadow-lg">
+                    <div class="card-body p-5">
+                        <form action="controller/upload.php" method="POST" enctype= "multipart/form-data">
+                            <div class="form-group">
+                                <h2 class="text-center">Upload Assignment</h2>  
+                                <input type="text" name="uploadAssignment" value="1" hidden>
+                                <div class="form-group">
+                                    <input type="text" class="form-control-file" name="assName" placeholder="Assignment Name" required="required">
+                                </div>
+                                <div class="form-group">
+                                    <input type="file" class="form-control-file" name="assUpload" required>
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-block">Upload</button> 
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
         <div class="col-md">
             <div style="margin: 50px auto;">
-                <div class="form">
-                    <div class="form-group"><br>
-                        <h2 class="text-center">Assignment Management</h2><br>
-                        <a href="submitted">
-                            <button type="submit" class="btn btn-primary btn-block">Student's Assignment</button> 
-                        </a>
-                        <br>
-                        <a href="view_assignment">
-                            <button type="submit" class="btn btn-primary btn-block">Given Assignment</button> 
-                        </a>
+                <div class="card shadow-lg">
+                    <div class="card-body p-5">
+                        <div class="form-group"><br>
+                            <h2 class="text-center">Assignment Management</h2><br>
+                            <a href="submitted">
+                                <button type="submit" class="btn btn-primary btn-block">Student's Assignment</button> 
+                            </a>
+                            <br>
+                            <a href="view_assignment">
+                                <button type="submit" class="btn btn-primary btn-block">Given Assignment</button> 
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -64,26 +70,28 @@ else:
     $assGiven = Assignment::getAssignment();
 ?>
 <div class="upload-form">
-    <div class="form">
-        <form action="controller/upload.php" method="POST" enctype= "multipart/form-data">
-            <div class="form-group">
-                <h2 class="text-center">Answer</h2>
+    <div class="card shadow-lg">
+        <div class="card-body p-5">
+            <form action="controller/upload.php" method="POST" enctype= "multipart/form-data">
                 <div class="form-group">
-                    <select class="form-control" name="assName" required>
-                        <?php foreach ($assGiven as $ass): ?>
-                            <option value='<?php echo $ass->getAssID(); ?>'><?php echo htmlspecialchars($ass->getAssName(), ENT_QUOTES, 'UTF-8'); ?></option>
-                        <?php endforeach; ?>
-                    </select>
+                    <h2 class="text-center">Answer</h2>
+                    <div class="form-group">
+                        <select class="form-control" name="assName" required>
+                            <?php foreach ($assGiven as $ass): ?>
+                                <option value='<?php echo $ass->getAssID(); ?>'><?php echo htmlspecialchars($ass->getAssName(), ENT_QUOTES, 'UTF-8'); ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <input type="file" class="form-control-file" name="assUpload" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Upload</button> 
                 </div>
-                <div class="form-group">
-                    <input type="file" class="form-control-file" name="assUpload" required>
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">Upload</button> 
-            </div>
-        </form>
-        <a href="view_assignment">
-        <button type="submit" class="btn btn-primary btn-block">Given Assignment</button>
-        </a>
+            </form>
+            <a href="view_assignment">
+            <button type="submit" class="btn btn-primary btn-block">Given Assignment</button>
+            </a>
+        </div>
     </div>
 </div>
 <?php endif; ?>
