c889682443b500735e3ed05320e8bfbef51368e1
Phạm Trương Minh Đức <phamtruongminhduc@gmail.com>
2021-10-31 21:33:33 +0700

    fix db

--- src/controller/connect.php
+++ src/controller/connect.php
@@ -2,8 +2,7 @@
 
 class dbConnect {
     public static function ConnectToDB() {
-        $dir = __DIR__;
-        return new PDO('sqlite:https://dashboard.heroku.com/models/database.db');
+        return new PDO('sqlite:'.__DIR__.'../models/database.db');
     }
 
     public static function Disconnect($con) {
