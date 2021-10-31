245e5be9fd4ccd384a82192002083c877d7ada50
Phạm Trương Minh Đức <phamtruongminhduc@gmail.com>
2021-10-31 21:35:19 +0700

    fix db

--- src/controller/connect.php
+++ src/controller/connect.php
@@ -2,7 +2,7 @@
 
 class dbConnect {
     public static function ConnectToDB() {
-        return new PDO('sqlite:'.__DIR__.'../models/database.db');
+        return new PDO('sqlite:'.__DIR__.'/models/database.db');
     }
 
     public static function Disconnect($con) {
